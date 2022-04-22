415e8419f7bd485ef56f75bc70c1660d65715ea7
SlightlyOffSync <79831737+SlightlyOffSync@users.noreply.github.com>
2022-04-23 00:01:51 +0800

    Create setup-moonraker.sh

---
+++ scripts/setup-moonraker.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+COL='\033[1;32m'
+NC='\033[0m' # No Color
+echo -e "${COL}Setting up Moonraker"
+
+read -p "Do you have \"Klipper\" installed? (y/n): " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo -e "${COL}\nPlease go to extensions and install Klipper${NC}"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+echo -e "${COL}Downloading Moonraker\n${NC}"
+curl -o moonraker.zip -L https://github.com/Arksine/moonraker/archive/refs/heads/master.zip
+
+echo -e "${COL}Extracting Moonraker\n${NC}"
+unzip moonraker.zip
+rm -rf moonraker.zip
+mv moonraker-master /moonraker
+
+echo -e "${COL}Installing dependencies...\n${NC}"
+# install required dependencies
+apk add --no-cache libcurl 
+apk update
+pip3 install -r /moonraker/scripts/moonraker-requirements.txt
+
+mkdir -p /root/extensions/moonraker
+cat << EOF > /root/extensions/klipper/manifest.json
+{
+        "title": "Moonraker plugin",
+        "description": "Requires OctoKlipper plugin"
+}
+EOF
+
+cat << EOF > /root/extensions/moonraker/start.sh
+#!/bin/sh
+python3 /moonraker/moonraker/moonraker.py /moonraker/docs/moonraker.conf -n
+EOF
+
+cat << EOF > /root/extensions/klipper/kill.sh
+#!/bin/sh
+pkill -f 'moonraker\.py'
+EOF
+chmod +x /root/extensions/moonraker/start.sh
+chmod +x /root/extensions/moonraker/kill.sh
+chmod 777 /root/extensions/moonraker/start.sh
+chmod 777 /root/extensions/moonraker/kill.sh
+
+echo -e "${COL}\nMoonraker installed! Please kill the app and restart it again to see it in extension settings${NC}"
